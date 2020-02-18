fd9d19dd0bc4f2c016fbf4f165f40eefa1683858
Karen Payne <paynekaren@comcast.net>
2020-02-18 09:25:54 -0800

    Added InvokeIfRequired extension

---
+++ WinFormExtensions/ControlExtensions.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Runtime.CompilerServices
+Imports System.Windows.Forms
+
+Public Module ControlExtensions
+    <Extension>
+    Public Sub InvokeIfRequired(control As Control, action As Action(Of Control))
+        If control.InvokeRequired Then
+            control.Invoke(New Action(Sub() action(control)))
+        Else
+            action(control)
+        End If
+    End Sub
+End Module
+
