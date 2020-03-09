a856d0181b2d29f0fc376d033cd066fbce9a8ba6
Karen Payne <paynekaren@comcast.net>
2020-03-09 07:24:39 -0700

    Added AppContext.BaseDirectory sample

---
+++ AppDomainConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AppDomainConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * In standard .NET Framework AppDomain.CurrentDomain.BaseDirectory
+             */
+            Console.WriteLine($"AppContext.BaseDirectory:\n{AppContext.BaseDirectory}");
+            Console.ReadLine();
+        }
+    }
+}
