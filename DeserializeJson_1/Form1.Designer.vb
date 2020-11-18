bb470b6fcfc64b5862265df17287cd80afffeb49
Karen Payne <Karen.1.payne@oregon.gov>
2020-11-18 12:28:27 -0800

    Semi simple json deserialization code sample

---
+++ DeserializeJson_1/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DeserializeButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DeserializeButton
+        '
+        Me.DeserializeButton.Location = New System.Drawing.Point(23, 32)
+        Me.DeserializeButton.Name = "DeserializeButton"
+        Me.DeserializeButton.Size = New System.Drawing.Size(148, 23)
+        Me.DeserializeButton.TabIndex = 0
+        Me.DeserializeButton.Text = "Deserialize json"
+        Me.DeserializeButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(213, 103)
+        Me.Controls.Add(Me.DeserializeButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DeserializeButton As Button
+End Class
---
+++ DeserializeJson_1/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+'
+' NuGet package below
+'
+Imports System.Text.Json
+
+Public Class Form1
+    Private Sub DeserializeButton_Click(sender As Object, e As EventArgs) Handles DeserializeButton.Click
+        Dim fileName = "json.json"
+        Dim json = File.ReadAllText(fileName)
+        Dim results As RecordRoot = JsonSerializer.Deserialize(Of RecordRoot)(json)
+    End Sub
+End Class
+'
+' Classes below to be in their own class files.
+'
+Public Class RecordRoot
+    Public Property records As Records
+End Class
+
+Public Class Records
+    Public Property record() As Record()
+End Class
+
+Public Class Record
+    Public Property _date As String
+    Public Property summary As String
+    Public Property issuetype As String
+    Public Property team As String
+    Public Property priority As String
+    Public Property path As String
+    Public Property caller As String
+    Public Property CIPFA_priority As String
+    Public Property person As String
+    Public Property details As String
+    Public Property allmessages As Allmessages
+    Public Property id As String
+    Public Property status As String
+End Class
+
+Public Class Allmessages
+    Public Property messages As Messages
+End Class
+
+Public Class Messages
+    Public Property message As Message
+End Class
+
+Public Class Message
+    Public Property msgSubject As String
+    Public Property msgTo As String
+    Public Property msgFrom As String
+    Public Property msgBody As String
+    Public Property msgDate As String
+End Class
+
